c05ddb11f6e0f4088ea8f7fb70198543759d670f
Joanna <aska3001@wp.pl>
2021-07-26 19:48:39 +0200

    desc

--- Bet.cs
+++ Bet.cs
@@ -8,5 +8,24 @@ namespace Lab1
 {
     public class Bet
     {
+        public int Amount; // Ilośc postawionych pieniędzy
+        public int Dog; // Numer psa, na którego postawiono
+        public Guy Bettor; // Facet, który zawarł zakład
+
+        public string GetDescription()
+        {
+            // Zwraca string, który określa, kto obstawił wyścig, jak dużo pieniędzy
+            // postawił i na którego psa ("Janek postawił 8 zł na psa numer 4").
+            // Jeżeli ilość jest równa zero, zakład nie został zawarty
+            // ("Janek nie zawarł zakładu")
+        }
+
+        public int PayOut(int Winner)
+        {
+            // Parametrem jest zwycięzca wyścigu. Jeżeli pies wygrał,
+            // zwróć wartość postawioną. W przeciwnym razie zwróć wartość
+            // postawioną ze znakiem minus
+        }
+
     }
 }
