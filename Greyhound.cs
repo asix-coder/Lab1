a9cb18c70f1f7743e2defcd57df1530dc353d11d
Joanna <aska3001@wp.pl>
2021-07-26 19:43:11 +0200

    Add project files.

---
+++ Greyhound.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public class Greyhound
+    {
+        public int StartingPosition; // Miejsce, gdzie zaczyna się PictureBox
+        public int RacetrackLength; // Jak długa jest trasa
+        public PictureBox MyPictureBox = null; // Mój obiekt PictureBox
+        public int Location = 0; // Moje położenie na torze wyścigowym
+        public Random MyRandom = 0; // Instancja klasy random
+
+        public bool Run()
+        {
+            // Przesuń się losowo o 1, 2, 3 lub 4 punkty
+            // Zaktualizuj położenie PictureBox na formularzu
+            // Zwróć true, jeżeli wygrałem wyścig
+        }
+
+        public void TakeStartingPosition()
+        {
+            // Wyzeruj położenie i ustaw na linii startowej
+        }
+    }
+}
---
+++ Guy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public class Guy
+    {
+        public string Name; // Imię faceta
+        public Bet MyBet; // Instancja klasy Bet przechowująca dane o zakładzie
+        public int Cash; // Jak dużo pieniędzy posiada
+        
+        // Ostatnie dwa pola są kontrolkami GUI na formularzu
+        public RadioButton MyRadioButton; // Moje pole wyboru
+        public Label MyLabel; // Moja etykieta
+
+        public void UpdateLabels()
+        {
+            // Ustaw moje pole tekstowe na opis zakładu, a napis obok
+            // pola wybru tak, aby pokazywał ilość pieniędzy ("Janek ma 43 zł")
+        }
+
+        public void ClearBet() { } // Wyczyść mój zakład, aby był równy zero
+
+        public void PlaceBet(int Amount, int DogToWin)
+        {
+            // Ustal nowy zakład i przechowaj go w polu MyBet
+            // Zwróć true, jeżeli facet ma wystarczającą ilość pieniędzy, aby obstawić
+        }
+
+        public void Collect(int Winner)
+        {
+            // Poproś o wypłatę zakładu i zaktualizuj etykiety
+        }
+    }
+}
